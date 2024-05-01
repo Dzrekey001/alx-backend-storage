4691e0fcbc5a58f44e94fa5c4b60d0d0cb398a3f
Dzrekey001 <dkdzrekey001@gmail.com>
2024-05-01 03:17:28 -0700

    Initial Commit

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Python function that inserts a new document
+    in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Returns the new _id after inserting a new document"""
+
+    return mongo_collection.insert_one(kwargs).insert_id
